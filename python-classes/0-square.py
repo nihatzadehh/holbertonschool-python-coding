d2f3359e1cbfe8a4c90127a45826548f040c91dc
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-23 17:14:36 +0400

    Update 0-square.py

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This is a script for my hs project"""
 class Square:
+    """This class creates an object that contains size"""
     def __init__(self, size):
         self.__size = size
