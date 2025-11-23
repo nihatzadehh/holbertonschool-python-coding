ff880a6c822d6377575d07ce5b9e77e58d4c6ba1
nihatzadehh <nihat.mmzade@gmail.com>
2025-11-23 17:30:36 +0400

    Create 2-square.py

---
+++ python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Just a task"""
+
+
+class Square:
+    "My class's documentation"
+    def __init__(self, size=0):
+        if isinstance(size, int):
+            if size >= 0:
+                self.__size = size
+            else:
+                raise ValueError("size must be >= 0")
+        else:
+            raise TypeError("size must be an integer")
+    def area(self):
+        return self.__size ** 2
